9a15d7496b09b3cc90bad7902146614d42a8bf49
tourn <latzer.daniel@gmail.com>
2022-04-24 14:03:02 +0200

    Add colorful puke!

---
+++ src/animations/sick.js
@@ -0,0 +1,213 @@
+duration = 100;
+setup.animations.register("sick",
+    [
+        {
+            duration: duration * 20,
+            image: [
+                "(⑊_/)",
+                "(O_o)",
+                "(> <)"
+            ],
+            mask: [
+                "hhhhh",
+                "hHHHh",
+                "hhhhh"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊_/)",
+                "( O_)",
+                "(> <)"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh",
+                "hhhhh"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "  /) ",
+                " ( O)",
+                "o( >)"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh",
+                "hhhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)",
+                "o( >)"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh",
+                "hhhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-",
+                "o( >)"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh1",
+                "hhhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-",
+                "o( >)⑊"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh1",
+                "hhhhh1"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-__",
+                "o( >)_⑊"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh111",
+                "hhhhh11"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-__",
+                "o( >)_⑊_"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh111",
+                "hhhhh111"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-__",
+                "o( >)_⑊⑊_"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh111",
+                "hhhhh1111"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X)-__",
+                "o( >)_⑊⑊__"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh111",
+                "hhhhh11111"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X) __",
+                "o( >)_⑊⑊__"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh 11",
+                "hhhhh11111"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  /) ",
+                " ( X) ",
+                "o( >)_⑊⑊__"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh ",
+                "hhhhh11111"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "  /) ",
+                " ( X) ",
+                "o( >)_▲▲___"
+            ],
+            mask: [
+                "hhhhh",
+                "hhHHh ",
+                "hhhhh111111"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊_/)",
+                "( X_) ",
+                "(> <)_▲▲___"
+            ],
+            mask: [
+                "hhhhh",
+                "hHHHh ",
+                "hhhhh111111"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊_/)",
+                "(X_x) ",
+                "(> <)_▲▲___"
+            ],
+            mask: [
+                "hhhhh",
+                "hHHHh ",
+                "hhhhh111111"
+            ]
+        },
+        {
+            duration: duration * 20,
+            image: [
+                "(⑊_/)",
+                "(O_o) ",
+                "(> <)_▲▲___"
+            ],
+            mask: [
+                "hhhhh",
+                "hHHHh ",
+                "hhhhh111111"
+            ]
+        },
+    ]
+);
