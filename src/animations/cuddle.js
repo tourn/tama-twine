bfba7659fe0f9491ca766592ae56e8b909e3d895
tourn <latzer.daniel@gmail.com>
2022-04-26 13:39:24 +0200

    Add some more animations

---
+++ src/animations/cuddle.js
@@ -0,0 +1,36 @@
+duration = 300;
+setup.animations.register("cuddle",
+    [
+        {
+            loop: 7,
+            frames: [
+                {
+                    duration: 300,
+                    image: [
+                        " (⑊_/)",
+                        " (^-^)",
+                        " (> <)"
+                    ],
+                    mask: [
+                        "hhhhh",
+                        "hHHHh",
+                        "hhhhh"
+                    ]
+                },
+                {
+                    duration: 300,
+                    image: [
+                        "     ",
+                        " (⑊_/)",
+                        "((^-^))",
+                    ],
+                    mask: [
+                        "     ",
+                        " hhhhh",
+                        "hhHHHhh",
+                    ],
+                },
+            ]
+        },
+    ]
+);
---
+++ src/animations/idle.js
@@ -0,0 +1,31 @@
+duration = 100;
+setup.animations.register("idle",
+[
+	{
+        duration: duration * 50,
+        image: [
+			"(⑊_/)",
+			"(O_o)",
+			"(> <)"
+        ],
+        mask: [
+			"hhhhh",
+			"hHHHh",
+			"hhhhh"
+        ]
+    },
+    {
+        duration: duration,
+        image: [
+			"(⑊_/)",
+			"(-_-)",
+			"(> <)"
+        ],
+        mask: [
+			"hhhhh",
+			"hHHHh",
+			"hhhhh"
+        ]
+    },
+]
+);
