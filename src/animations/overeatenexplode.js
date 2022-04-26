f251f884990b08df1c2514ebd4938f99903bca35
tourn <latzer.daniel@gmail.com>
2022-04-26 13:27:27 +0200

    Explosions!

---
+++ src/animations/overeatenexplode.js
@@ -0,0 +1,665 @@
+duration = 100;
+setup.animations.register("overeatenexplode",
+    [
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊__/)         _",
+                "(o__o)       _|_|_",
+                "(>  <)      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh          ",
+                "hHHHHh            ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊__/)         _",
+                "( o__)       _|_|_",
+                "(>  <)      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh          ",
+                "hhHHHh            ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "(⑊__/)         _",
+                "( -__)       _|_|_",
+                "(>  <)      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhHHHh      ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊__/)         _",
+                "( O__)       _|_|_",
+                "(>  <)      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhHHHh      ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "(⑊__/)         _",
+                "( ^__)       _|_|_",
+                "(>  <)      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhHHHh      ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "(⑊__/)         _",
+                "(  ^_)       _|_|_",
+                "( >  >      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhHHHh      ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "   /)          _",
+                "(   ^)       _|_|_",
+                "(  > )      |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhHHHh      ",
+                "hhhhhh       rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "    /)         _",
+                " (   ^)      _|_|_",
+                "O(  > )     |^^^^^|"
+            ],
+            mask: [
+                "hhhhhh      ",
+                "hhhhhHh     ",
+                "hhhhhhh      rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "     /)        _",
+                "  (   ^)     _|_|_",
+                " O(  > )    |^^^^^|"
+            ],
+            mask: [
+                "    hhh         ",
+                "  hhhhHh          ",
+                " hhhhhhhh    rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "      /)       _",
+                "   (   ^)    _|_|_",
+                "  O(  > )   |^^^^^|"
+            ],
+            mask: [
+                "      hh        ",
+                "   hhhhHh         ",
+                "  hhhhhhh    rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "       /)      _",
+                "    (   ^)   _|_|_",
+                "   O(  > )  |^^^^^|"
+            ],
+            mask: [
+                "       hh       ",
+                "    h   Hh        ",
+                "   hh  hhh   rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "        /)     _",
+                "     (   ^)  _|_|_",
+                "    O(  > ) |^^^^^|"
+            ],
+            mask: [
+                "        hh      ",
+                "     h   Hh       ",
+                "    hh  hhh  rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "         /)    _",
+                "      (   ^) _|_|_",
+                "     O(  > )|^^^^^|"
+            ],
+            mask: [
+                "         hh  ",
+                "      h   Hh ",
+                "     hhhhhhh rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "         /)    _",
+                "      (   ^)  |_|_",
+                "     O(   >)|^^^^^|"
+            ],
+            mask: [
+                "         hh  ",
+                "      h   Hh ",
+                "     hh   hh rrrrr "
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "         /)    _",
+                "      (   ^) _|_|_",
+                "     O(   >)|^^^^^|"
+            ],
+            mask: [
+                "         hh ",
+                "      h   Hh",
+                "     hh   hh rrrrr "
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "         /)    _",
+                "      (   ^) _|_|_",
+                "     O(    >|^^^^^|"
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hh    h rrrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)   _",
+                "      (   ^)_|_|_",
+                "     O(   >/^^^^^|"
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hh   h rrrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)  _",
+                "      (   ^)|_|_",
+                "     O(  >`^^^^^|"
+            ],
+            mask: [
+                "         hh   ",
+                "      h   Hh    ",
+                "     hh  h rrrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /) _",
+                "      (   ^|_|_",
+                "     O(  >`'^^^|"
+            ],
+            mask: [
+                "         hh  ",
+                "      h   H    ",
+                "     hh  hrrrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)_",
+                "      (   ^)|_",
+                "     O(  >'`^^|"
+            ],
+            mask: [
+                "         hh ",
+                "      h   Hh  ",
+                "     hh  hrrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)",
+                "      (   ^|_",
+                "     O(  >-'^|"
+            ],
+            mask: [
+                "         hh",
+                "      h   H",
+                "     hh  hrrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)",
+                "      (   ^)",
+                "     O(  >`'|"
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hh  hrr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)",
+                "      (   ^),",
+                "     O(  >`⑊"
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hh  hr "
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "         /)",
+                "      (   ^)",
+                "     O(  >⑊)'"
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hh  h h"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "         /)",
+                "      (   ^)",
+                "     O(  > )."
+            ],
+            mask: [
+                "         hh",
+                "      h   Hh",
+                "     hhhhhhh"
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "        /)/)",
+                "      (  ^_)",
+                "     O(  > )."
+            ],
+            mask: [
+                "        hhhh",
+                "      hhhHHh",
+                "     hhhhhhh"
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "       /)/)",
+                "     (  ^_)",
+                "    O(  > ) ."
+            ],
+            mask: [
+                "       hhhh",
+                "     hhhHHh",
+                "    hhhhhhh"
+            ]
+        },
+        {
+            duration: duration * 5,
+            image: [
+                "      ()_/)",
+                "     ( ^__)",
+                "    O( >  > _"
+            ],
+            mask: [
+                "      hhhhh",
+                "     hhHHHh",
+                "    hhhhhhh"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "    (⑊___/)",
+                "    (^___^)",
+                "    (>   <) _"
+            ],
+            mask: [
+                "    hhhhhhh",
+                "    hHHHHHh",
+                "    hhhhhhh"
+            ]
+        },
+        {
+            duration: duration * 3,
+            image: [
+                "    (⑊___/)",
+                "    (O___O)",
+                "    (>   <) _"
+            ],
+            mask: [
+                "    hhhhhhh",
+                "    hHHHHHh",
+                "    hhhhhhh"
+            ]
+        },
+        {
+            loop: 5,
+            frames: [
+                {
+                    duration: duration * 2,
+                    image: [
+                        "    (⑊___/)",
+                        "    (O___O)",
+                        "    (>   <)"
+                    ],
+                    mask: [
+                        "    hhhhhhh",
+                        "    hHHHHHh",
+                        "    hhhhhhh"
+                    ]
+                },
+                {
+                    duration: duration * 2,
+                    image: [
+                        "   (⑊_____/)",
+                        "   (O_____O)",
+                        "   (>     <)"
+                    ],
+                    mask: [
+                        "   hhhhhhhhh",
+                        "   hHHHHHHHh",
+                        "   hhhhhhhhh"
+                    ]
+                },
+            ]
+        },
+        {
+            loop: 5,
+            frames: [
+                {
+                    duration: duration * 2,
+                    image: [
+                        "    (⑊___/)",
+                        "    (>___<)",
+                        "    (>   <)"
+                    ],
+                    mask: [
+                        "    hhhhhhh",
+                        "    hHHHHHh",
+                        "    hhhhhhh"
+                    ]
+                },
+                {
+                    duration: duration * 2,
+                    image: [
+                        "   (⑊_____/)",
+                        "   (>_____<)",
+                        "   (>     <)"
+                    ],
+                    mask: [
+                        "   hhhhhhhhh",
+                        "   hHHHHHHHh",
+                        "   hhhhhhhhh"
+                    ]
+                },
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   /|__ __|⑊",
+                "   (>__-__<)",
+                "   (>  -  <)"
+            ],
+            mask: [
+                "   hhhh hhhh",
+                "   hHHH%HHHh",
+                "   hhhh%hhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "    /|___|⑊",
+                "   (X__-__X)",
+                "  (>       <)"
+            ],
+            mask: [
+                "    hhh%hhh",
+                "   hHHH%HHHh",
+                "  hh       hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   (X__ __X)",
+                "   (__   __)",
+                " (>         <)"
+            ],
+            mask: [
+                "   hHHh hHHh",
+                "   hh%  hhhh",
+                " hh         hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   (X_   _X)",
+                "  (__     __)",
+                "(>           <)"
+            ],
+            mask: [
+                "   hHH   HHh",
+                "  hh%     %hh",
+                "hh           hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   (X_   _X)",
+                "  (__     __)",
+                ">             <)"
+            ],
+            mask: [
+                "   hHH   HHh",
+                "  hh%     %hh",
+                "h             hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   (X-   -X)",
+                "            ",
+                ">(--       --) <)"
+            ],
+            mask: [
+                "   hHH   HHh",
+                "            ",
+                "hhh%       %hhhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "   (X-   -X)",
+                "            ",
+                " (__       __)  <)"
+            ],
+            mask: [
+                "   hHH   %Hh",
+                "            ",
+                " hh%       %hhhhhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "  (X-     -X)",
+                "            ",
+                " (__       __)  <)"
+            ],
+            mask: [
+                "  hHH     %Hh",
+                "            ",
+                " hh%       %hh hhh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "    _     _",
+                "  (X       X)",
+                " (__       __)  <)"
+            ],
+            mask: [
+                "    h     %",
+                "  hH       Hh",
+                " hh%       %hh  hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "",
+                "  (X-     -X)",
+                " (__       __)  <)"
+            ],
+            mask: [
+                "",
+                "  hHh     %Hh",
+                " hh%       %hh  hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "",
+                "  (X-     -X)",
+                "(__        __)   <)"
+            ],
+            mask: [
+                "",
+                "  hHh     %Hh",
+                "hh%        %hh   hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "",
+                "    _     _",
+                "(_(X       X)_)   <)"
+            ],
+            mask: [
+                "",
+                "    h     %",
+                "h%hH       Hh%h   hh"
+            ]
+        },
+        {
+            duration: duration,
+            image: [
+                "",
+                "",
+                "___X-     -X__)   <)"
+            ],
+            mask: [
+                "",
+                "",
+                "hh%Hh     %H%hh   hh"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "",
+                "",
+                "___X_     _X__)   <)"
+            ],
+            mask: [
+                "",
+                "",
+                "hh%Hh     %H%hh   hh"
+            ]
+        },
+        {
+            duration: duration * 2,
+            image: [
+                "",
+                "",
+                "___X_     _o__)   <)"
+            ],
+            mask: [
+                "",
+                "",
+                "hh%Hh     %H%hh   hh"
+            ]
+        },
+        {
+            duration: duration * 10,
+            image: [
+                "",
+                "",
+                "___X_     _X__)   <)"
+            ],
+            mask: [
+                "",
+                "",
+                "hh%Hh     %H%hh   hh"
+            ]
+        },
+    ]
+);
